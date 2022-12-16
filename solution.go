bcd2717cbe29ca200374a95147c094d47ef3ed14
dilmurodov <tolibdilmurodov98@gmail.com>
2022-12-16 12:58:04 +0500

    solution complated for les-00

--- solution.go
+++ solution.go
@@ -7,7 +7,7 @@ import (
 )
 
 func GetMessage() string {
-	return emoji.Sprint("Hello :world_map:!")
+	return emoji.Sprint("Hello :world_map:")
 }
 
 func CalcSquare(sideLen float64, sidesNum int32) float64{
