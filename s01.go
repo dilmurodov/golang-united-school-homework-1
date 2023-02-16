9f8980de35e83543b7d552fc0ddbfd5d9744822a
dilmurodov <tolibdilmurodov98@gmail.com>
2023-02-16 10:22:23 +0500

    [*] task-2

--- s01.go
+++ s01.go
@@ -5,3 +5,38 @@ type UserInterface interface {
 	SetLastName(string)
 	FullName() string
 }
+
+type User struct {
+	firstName string
+	lastName  string
+}
+
+func New() UserInterface {
+	return &User{}
+}
+
+func (u *User) SetFirstName(firstName string) {
+	u.firstName = firstName
+}
+
+func (u *User) SetLastName(lastName string) {
+	u.lastName = lastName
+}
+
+func (u *User) FullName() string {
+	return u.firstName + " " + u.lastName
+}
+
+func ResetUser(user UserInterface) {
+	user.SetFirstName("")
+	user.SetLastName("")
+}
+
+func IsUser(input interface{}) bool {
+	return input.(UserInterface) != nil
+}
+
+func ProcessUser(input UserInterface) string {
+	input.SetFirstName("John")
+	return input.FullName()
+}
