a23e4a6223dddb720aee86dc233d7bab1badca03
dilmurodov <tolibdilmurodov98@gmail.com>
2023-03-02 00:00:25 +0500

    [*] finished task

--- hard.go
+++ hard.go
@@ -20,5 +20,63 @@ input: (al)G(al)()()G
 output: alGalooG
 */
 func GoalParsers(strReader *strings.Reader) string {
-	panic("Not implemented yet...")
+
+	var (
+		readerBuf = make([]byte, 1024)
+	)
+
+	_, err := strReader.Read(readerBuf)
+	if err != nil {
+		panic(err)
+	}
+	parsedString := parse(string(readerBuf))
+	return parsedString
+}
+
+func parse(word string) string {
+	output := ""
+	start := -1
+
+	for i, char := range word {
+		if char == 'G' {
+			if start != -1 {
+				return ""
+			}
+			output += "G"
+		} else if char == '(' {
+			if start != -1 {
+				return ""
+			}
+			start = i
+		} else if char == ')' {
+			if start == -1 {
+				return ""
+			}
+			token := word[start : i+1]
+			if token == "()" {
+				output += "o"
+			} else if token == "(al)" {
+				output += "al"
+			} else {
+				return ""
+			}
+			start = -1
+		} else if char == 'a' {
+			if start == -1 {
+				return ""
+			}
+		} else if char == 'l' {
+			if start == -1 || len(output) == 0 || output[len(output)-1] != 'a' {
+				return ""
+			}
+		} else {
+			return ""
+		}
+	}
+
+	if start != -1 {
+		return ""
+	}
+
+	return output
 }
